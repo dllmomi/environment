15ca5123abfdae5047daab2b5cc2507226ebe328
dllmomi <dlmforce@gmail.com>
2018-09-21 19:01:06 +0900

    ターミナルカラーを solarized にできるようにインストールするコマンドを追加した

--- mac_customize.sh
+++ mac_customize.sh
@@ -6,3 +6,8 @@ defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
 killall Dock
+
+# configuration environment for terminal
+brew install coreutils
+git clone https://github.com/tomislav/osx-terminal.app-colors-solarized.git ~/.osx-terminal.app-colors-solarized    # loading for terminal environment configuration
+git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors-solarized    # after install, add entry for .zshrc.local to "alias ls='gls --color=auto"
