e144e71b6f102842b3873defa7a520fe5acc5203
dllmomi <dlmforce@gmail.com>
2018-10-30 21:03:15 +0900

    update mac_customize.sh if re-setup situation

--- mac_customize.sh
+++ mac_customize.sh
@@ -5,9 +5,16 @@ defaults write com.apple.finder FinderSounds -bool no
 defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
+killall Finder
 killall Dock
 
-# configuration environment for terminal
-brew install coreutils
-git clone https://github.com/tomislav/osx-terminal.app-colors-solarized.git ~/.osx-terminal.app-colors-solarized    # loading for terminal environment configuration
-git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors-solarized    # after install, add entry for .zshrc.local to "alias ls='gls --color=auto"
+# customize development environments
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git openssl tmux coreutils
+
+# loading for terminal environment configuration
+# after install, 
+#   * load profile on terminal settings. if folder hidden, push 'command + shift + .'
+#   * add entry for .zshrc.local to "alias ls='gls --color=auto"
+git clone https://github.com/tomislav/osx-terminal.app-colors-solarized.git ~/.osx-terminal.app-colors-solarized
+git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors-solarized
